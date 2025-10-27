c7ba693a3381517d199750b472b352e11da668f4
Abdellah-saim-mamoune1 <abdellahsaimmamoune1@gmail.com>
2025-10-27 19:02:25 +0100

    Update API.ts

--- src/APIs/API.ts
+++ src/APIs/API.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://uni-clubs-bgg3hjfdcrghawag.spaincentral-01.azurewebsites.net/api/", // your .NET backend URL
+  baseURL: "https://clubsservice-d6e7hccybzcdhefm.spaincentral-01.azurewebsites.net/api/", // your .NET backend URL
   withCredentials: true, // since you’re using cookies/JWT
 });
 
